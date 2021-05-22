4a3b48cd4df9057efc2cbf85bc9cc36df2fc97c4
Vincent Chee <vincent.kh.chee@gmail.com>
2021-05-22 19:34:16 +0800

    RPSInstance WIP
    - messages added to require statements
    - withdrawWinnings function implemented
    - isActive set to true in game enrollment

--- contracts/RockPaperScissorsInstance.sol
+++ contracts/RockPaperScissorsInstance.sol
@@ -19,32 +19,43 @@ contract RockPaperScissorsInstance is Initializable {
   // TODO: Maybe have events so that client can see all RPS games.
 
   modifier isNonZeroAddress(address _address) {
-    require(_address != address(0));
+    require(_address != address(0), "This is an empty address.");
     _;
   }
 
   modifier isNewPlayer() {
-    require(msg.sender != playerA && msg.sender != playerB);
+    require(msg.sender != playerA && msg.sender != playerB, "You are already a part of this game.");
     _;
   }
 
   modifier isValidPlayer(address _address) {
-    require(msg.sender == playerA || msg.sender == playerB);
+    require(msg.sender == playerA || msg.sender == playerB, "You are not a part of this game.");
+    _;
+  }
+  
+  modifier isOpenGame() {
+    require(playerB == address(0), "This game is full.");
+    _;
+  }
+
+  modifier canWithdrawWinnings() {
+    require(msg.sender == winner && isActive == false, "You are not allowed to withdraw.");
     _;
   }
 
 
-  function initialize(address _creatorPlayer, uint _betAmount) public initializer {
+  function initialize(address _creatorPlayer, IERC20 _token, uint _betAmount) public initializer {
+    token = _token;
     playerA = _creatorPlayer;
     betAmount = _betAmount;
     depositBet(_betAmount);
   }
 
-  function enrollInGame(uint _betAmount) public isNewPlayer() isNonZeroAddress(msg.sender) {
-    require(playerB == address(0));
-    require(_betAmount == betAmount);
+  function enrollInGame(uint _betAmount) public isNewPlayer() isNonZeroAddress(msg.sender) isOpenGame() {
+    require(_betAmount == betAmount, "you've submitted the incorrect bet amount.");
     depositBet(_betAmount);
     playerB = msg.sender;
+    isActive = true;
   }
 
   function submitMove(uint move) public isValidPlayer(msg.sender) {
@@ -59,19 +70,20 @@ contract RockPaperScissorsInstance is Initializable {
     }
   }
 
-  function withdrawWinnings() public {
+  function withdrawWinnings() public canWithdrawWinnings() {
+    uint winningAmount = betAmount * 2;
+    require(winningAmount > 0, "There is nothing to withdraw.");
+    token.approve(msg.sender, winningAmount);
+    token.transferFrom(address(this), msg.sender, winningAmount);
   }
 
   function depositBet(uint _depositBetAmount) internal {
     if (betAmount > 0) {
-      token.approve(msg.sender, _depositBetAmount);
+      require(token.allowance(msg.sender, address(this)) == _depositBetAmount, "You don't have allowance.");
       token.transferFrom(msg.sender, address(this), _depositBetAmount);
     }
   }
 
-  // 0 = rock
-  // 1 = paper
-  // 2 = scissors
   function calculateGameState() internal view returns (address) {
     bytes32 hashedRock = keccakUint(0);
     bytes32 hashedPaper = keccakUint(1);
@@ -91,9 +103,15 @@ contract RockPaperScissorsInstance is Initializable {
   }
 
   function endGame(address _winningAddress) internal {
-    // the logic for ending the game
-    // based on the winning address, we allow this person to withdraw the tokens deposited into this contract
-    // for their victory    
+    if (_winningAddress == address(0)) {
+      delete playerAMove;
+      delete playerBMove;
+    }
+
+    if (_winningAddress != address(0)) {
+      isActive = false;
+      winner = _winningAddress;
+    }
   }
 
   function keccakUint(uint _int) internal pure returns (bytes32) {
